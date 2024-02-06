64d307185c21ebbc8c5d838a1e16247ebbbaee21
Satya Selvan <satyaat92@gmail.com>
2024-02-06 16:32:44 +0800

    database connection debug

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://socialNetworkAPI:Howkey%402011@tpcbr-4-project.ou5hsth.mongodb.net/book_search_db_2?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://satyaat:bootcamp@92@cluster0.oawpbse.mongodb.net/Mern-book-search?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
